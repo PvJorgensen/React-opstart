15225c6b88a9f7df9e5a79d32a7944218656e4c7
Patrik_Jørgensen <vadmand806@gmail.com>
2024-01-10 09:41:17 +0100

    main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,7 +3,7 @@ import { PageTitle } from "../PageTitle/PageTitle"
 export const Main = () => {
     return (
         <main>
-            <PageTitle pagetitle="Velkommen til min side - nu med en sidetitle styret af props"></PageTitle>
+            <PageTitle pagetitle="Dette er min pagetitle" subtitle="Dette er min subtitle"></PageTitle>
             <p>Her tester vi en react app</p>
         </main>
     )
--- src/components/PageTitle/PageTitle.jsx
+++ src/components/PageTitle/PageTitle.jsx
@@ -1,5 +1,8 @@
 export const PageTitle = props => {
     return (
+        <>
         <h1>{props.pagetitle}</h1>
+        <h2>{props.subtitle}</h2>
+        </>
     )
 }
